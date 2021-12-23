25cc394371b0559c5359ce000ca7cf127c7b9815
Bo Zhang <bo@bytelegend.com>
2021-12-23 14:22:52 +0800

    Support opening markdown link in same workspace
    If a markdown link points to a file in same workspace,
    e.g. https://github.com/CURRENT/WORKSPACE/blob/main/PATH/TO/FILE,
    it can be recognized as `github1s:/PATH/TO/FILE`

--- src/vs/editor/common/config/editorOptions.ts
+++ src/vs/editor/common/config/editorOptions.ts
@@ -4302,7 +4302,9 @@ export const EditorOptions = {
 		EditorOption.disableMonospaceOptimizations, 'disableMonospaceOptimizations', false
 	)),
 	domReadOnly: register(new EditorBooleanOption(
-		EditorOption.domReadOnly, 'domReadOnly', false,
+		// below codes are changed by github1s
+		EditorOption.domReadOnly, 'domReadOnly', !!platform.isMobile,
+		// above codes are changed by github1s
 	)),
 	dragAndDrop: register(new EditorBooleanOption(
 		EditorOption.dragAndDrop, 'dragAndDrop', true,
